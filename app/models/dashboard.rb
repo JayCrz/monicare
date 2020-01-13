d35d41137b864d08a4c067163101526c706ca587
beckhsu <justin7610@gmail.com>
2020-01-13 19:13:53 +0800

    finished log notifi

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,6 +1,7 @@
 class Dashboard < ApplicationRecord
   enum category: [:medicine, :eat, :misc]
   belongs_to :child
+  has_many :notifications, dependent: :destroy
   mount_uploader :parent_sign, ParentSignUploader
   mount_uploader :admin_sign, AdminSignUploader
 end
