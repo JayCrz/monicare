d47491fffd4485d9b9249e053c64094d5715b146
beckhsu <justin7610@gmail.com>
2020-01-14 20:29:58 +0800

    add FBlogin

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -1,7 +1,6 @@
 class Dashboard < ApplicationRecord
   enum category: [:medicine, :eat, :misc]
   belongs_to :child
-  has_many :notifications, dependent: :destroy
   mount_uploader :parent_sign, ParentSignUploader
   mount_uploader :admin_sign, AdminSignUploader
 end
