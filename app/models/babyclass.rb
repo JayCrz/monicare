6aed3272309491cef2b6ad516ff495f91abc16f7
yalan <lanya4190@gmail.com>
2020-01-14 12:54:53 +0800

    validation(WIP)

--- app/models/babyclass.rb
+++ app/models/babyclass.rb
@@ -1,4 +1,7 @@
 class Babyclass < ApplicationRecord
+  # validations
+  validates :nursery_class, presence: true
+  # relationship
   has_many :children
   has_many :class_teachers
   has_many :teachers, through: :class_teachers
