6168741ca95273c11be3df1b73d9cc7b0b75b128
Jay <ifiwtbsiwbs@gmail.com>
2020-01-15 11:45:12 +0800

    change notification to medicine controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def notification_read_url(notification)
+    "/teacher/dashboard/children/#{notification.child_id}/#{notification.user.children.find(notification.child_id).dashboards.find(notification.dashboard_id).category}/#{notification.dashboard_id}"
+  end
 end
