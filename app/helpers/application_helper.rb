3f7da5929b5f9f6efb92aab682edf3843c8d176c
Jay <ifiwtbsiwbs@gmail.com>
2020-01-16 04:57:05 +0800

    add pick up notification

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,9 @@
 module ApplicationHelper
   def notification_read_url(notification)
-    "/teacher/dashboard/children/#{notification.child_id}/#{notification.user.children.find(notification.child_id).dashboards.find(notification.dashboard_id).category}/#{notification.dashboard_id}"
+    if notification.dashboard_id != nil
+     "/teacher/dashboard/children/#{notification.child_id}/#{notification.user.children.find(notification.child_id).dashboards.find(notification.dashboard_id).category}/#{notification.dashboard_id}"
+    else
+      "/teacher/dashboard/children/#{notification.child_id}/pick_up/#{notification.pick_up_id}"
+    end
   end
 end
