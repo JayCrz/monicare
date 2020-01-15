2bfb30e2496f4e418f92c779dfe6f8ae0047ec04
Jay <ifiwtbsiwbs@gmail.com>
2020-01-16 04:57:05 +0800

    add pick up notification

--- app/controllers/teacher/pick_up_controller.rb
+++ app/controllers/teacher/pick_up_controller.rb
@@ -2,5 +2,6 @@ class Teacher::PickUpController < BabyclassAppliciationController
   def show
     @student = Child.find(params[:child_id])
     @pick_up = @student.pick_ups.find(params[:id])
+    Notification.where(child_id: params[:child_id], pick_up_id: params[:id]).update(teacher_read: true, user_read: true)
   end
 end
