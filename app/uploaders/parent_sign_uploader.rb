38788dc5c2eda046ec313781ab243489a3a7d740
yalan <lanya4190@gmail.com>
2020-01-16 13:14:49 +0800

    modify signature size and dashboard

--- app/uploaders/parent_sign_uploader.rb
+++ app/uploaders/parent_sign_uploader.rb
@@ -11,7 +11,7 @@ class ParentSignUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
-  process resize_to_fit: [400, 200]
+  process resize_to_fit: [300, 200]
 
   def extension_whitelist
     %w(jpg jpeg gif png)
