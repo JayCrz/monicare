8e91c0602f691443a6e9057bbcde89cc399c2006
beckhsu <justin7610@gmail.com>
2020-01-18 01:58:55 +0800

    googlelogin

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,23 +1,18 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-   def facebook
-    @user = User.from_omniauth(request.env["omniauth.auth"])  # 這邊會呼叫我們在 user 中定義的創建行為
+  
+   def google_oauth2
+    @user = User.from_omniauth(request.env["omniauth.auth"])
 
     if @user.persisted?
-      sign_in_and_redirect @user, :event => :authentication
-      set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
+      sign_in_and_redirect @user, event: :authentication 
+      set_flash_message(:notice, :success, kind: "Google") if is_navigational_format?
     else
-      
-      if @user.email.nil?
-        # 如果 User 沒有提供 email 刪除 User 的 app 讓 User 重新授權
-      
-        redirect_to new_user_registration_url, alert: "需要您同意 Email 授權唷！"
-      else
-        session["devise.facebook_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
     end
   end
+
   def failure
-    redirect_to new_user_session_path, alert: "無法獲得驗證！"
+    redirect_to root_path
   end
 end
